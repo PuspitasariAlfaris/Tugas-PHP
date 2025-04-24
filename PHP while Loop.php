c9e987ccf9b4f4c53150bc6a77772323dbfc464e
PuspitasariAlfaris <puspitasarialfaris0@gmail.com>
2025-04-24 21:43:19 +0800

    Add files via upload

---
+++ PHP while Loop.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+$i = 1;
+
+while ($i < 6) {
+  echo $i;
+  $i++;
+} 
+?>  
+
+<?php  
+$i = 1;
+
+while ($i < 6) {
+  if ($i == 3) break;  
+  echo $i;
+  $i++;
+} 
+?>  
+
+<?php  
+$i = 0;
+
+while ($i < 6) {
+  $i++;
+  if ($i == 3) continue;  
+  echo $i;
+} 
+?>  
+
+$i = 1;
+
+while ($i < 6):
+  echo $i;
+  $i++;
+endwhile;
+?> 
+
+</body>
+</html>
